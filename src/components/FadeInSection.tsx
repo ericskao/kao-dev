68f6dc95dab013755c45616a577d224bd99233d2
Eric K <ericskao@gmail.com>
2023-02-27 11:41:53 -0800

    only fade in once

--- src/components/FadeInSection.tsx
+++ src/components/FadeInSection.tsx
@@ -1,14 +1,18 @@
 import { useEffect, useRef, useState } from 'react';
+
 import './FadeInSection.scss';
 
 const FadeInSection = ({ children }: { children: React.ReactNode }) => {
-  const [isVisible, setVisible] = useState<boolean>(true);
-
+  const [isVisible, setVisible] = useState<boolean>(false);
   const domRef = useRef<any>();
 
   useEffect(() => {
     const observer: any = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => setVisible(entry.isIntersecting));
+      entries.forEach((entry) => {
+        if (!isVisible && entry.isIntersecting) {
+          setVisible(entry.isIntersecting);
+        }
+      });
     });
     observer.observe(domRef.current);
     return () => domRef.current && observer?.unobserve(domRef.current);
