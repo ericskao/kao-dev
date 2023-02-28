71712794938654a66e872095ab1c0ba3035e7927
Eric K <ericskao@gmail.com>
2023-02-27 20:39:54 -0800

    hamburger transitions

--- src/components/MobileNavBar.tsx
+++ src/components/MobileNavBar.tsx
@@ -1,9 +1,9 @@
 import LinkedIn from '../images/svgs/LinkedIn';
 import Twitter from '../images/svgs/Twitter';
 import { linkedInLink, PageLinkType, twitterLink } from './PageLayout';
+import { Link } from 'gatsby';
 
 import './MobileNavBar.scss';
-import { Link } from 'gatsby';
 
 interface MobileNavType {
   open: boolean;
@@ -15,28 +15,26 @@ const MobileNavBar = ({ open, links }: MobileNavType) => {
     <nav className="mobile-nav">
       {/* if open, set height and show items */}
       {/* if close, set height to 0 */}
-      {open && (
-        <ul className={open ? 'open' : 'close'}>
-          {links.map((link, index) => {
-            return (
-              <li key={index}>
-                <Link to={link.url}>{link.text}</Link>
-              </li>
-            );
-          })}
-          <li>
-            <a className="mobile-nav__social" target="_blank" href={twitterLink}>
-              <Twitter />
-            </a>
-          </li>
-          <li>
-            <a className="mobile-nav__social" target="_blank" href={linkedInLink}>
-              <LinkedIn />
-            </a>
-          </li>
-          <hr className="mobile-nav__hr" />
-        </ul>
-      )}
+      <ul className={open ? 'open' : 'close'}>
+        {links.map((link, index) => {
+          return (
+            <li key={index}>
+              <Link to={link.url}>{link.text}</Link>
+            </li>
+          );
+        })}
+        <li>
+          <a className="mobile-nav__social" target="_blank" href={twitterLink}>
+            <Twitter />
+          </a>
+        </li>
+        <li>
+          <a className="mobile-nav__social" target="_blank" href={linkedInLink}>
+            <LinkedIn />
+          </a>
+        </li>
+        <hr className="mobile-nav__hr" />
+      </ul>
     </nav>
   );
 };
